f2b71998a1aa650c5af9f09a7f3890210de07554
Michael Fearnley <44407940+FearMichael@users.noreply.github.com>
2019-09-05 21:01:54 -0400

    update

--- concert.js
+++ concert.js
@@ -5,32 +5,31 @@ const fs = require("fs");
 
 const getBandInfo = (searchItem) => {
     axios.get("https://rest.bandsintown.com/artists/" + encodeURI(searchItem) + "/events?app_id=" + process.env.bandsid)
-    .then(function(info) {
-        let concerts = Object.values(info.data);
-        if (concerts) {
-            Object.values(concerts).slice(0, 3).forEach(elem => {
-                console.log(`
+        .then(function (info) {
+            let concerts = Object.values(info.data);
+            // console.log(concerts);
+            if (concerts) {
+                concerts.slice(0, 3).forEach(elem => {
+                    console.log(`
                 Venue: ${elem.venue.name} 
                 Venue Location: ${elem.venue.city}, ${elem.venue.region}
                 Lat / Long: ${elem.venue.latitude} / ${elem.venue.longitude}
                 Event Date: ${moment(elem.datetime).format("MM/DD/YYYY @ hh:mm a ")}
-                Get Tickets? ${elem.offers[0].url}
                 `);
-            });
-            fs.appendFile("log.txt", `Concert searched: ${searchItem}\n`,function (err) 
-            {
-                if (err) {
-                    console.log(err)
-                }
-            });
-        } else {
-            console.log("Looks like that's not available... try another concert.")
-        };
-    }).catch(function(err) {
-        if (err) {
-            console.log(err);
-        };
-    });
+                });
+                fs.appendFile("log.txt", `Concert searched: ${searchItem}\n`, function (err) {
+                    if (err) {
+                        console.log(err)
+                    }
+                });
+            } else {
+                console.log("Looks like that's not available... try another concert.")
+            };
+        }).catch(function (err) {
+            if (err) {
+                console.log(err);
+            };
+        });
 };
 module.exports = {
     getBandInfo
